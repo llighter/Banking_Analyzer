a9def83658a1e5718bc3d38dbc1075ddb7ac9584
박윤하 <llighter@yunha-Macmini.local>
2021-09-25 10:50:45 +0900

    initial commit

---
+++ src/main/java/com/llighter/demo/App.java
@@ -0,0 +1,13 @@
+package com.llighter.demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
